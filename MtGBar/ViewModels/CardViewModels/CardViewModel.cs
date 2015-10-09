9f8e9b27259ce10ddd4b088590ad4c320a8885d2
Ben Stein <ben.s.stein@gmail.com>
2015-10-09 12:46:53 -0500

    Added a missing file.

---
+++ MtGBar/ViewModels/CardViewModels/CardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Melek.Domain;
+
+namespace MtGBar.ViewModels
+{
+    public class CardViewModel : ICardViewModel
+    {
+        public Card Card { get; set; }
+        public Printing Printing { get; set; }
+    }
+}
