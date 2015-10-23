1a80e321123649ac27e1affb08dac958c56e87ed
Ben Stein <ben.s.stein@gmail.com>
2015-10-23 16:06:52 -0500

    Added a missing file.

---
+++ MtGBar/Views/Controls/ManaCostView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Melek.Domain;
+
+namespace MtGBar.Views.Controls
+{
+    public partial class ManaCostView : UserControl
+    {
+        public ManaCostView()
+        {
+            InitializeComponent();
+            LayoutRoot.DataContext = this;
+        }
+        
+        public CardCostCollection Cost
+        {
+            get { return (CardCostCollection)GetValue(CostProperty); }
+            set { SetValue(CostProperty, value); }
+        }
+        
+        public static readonly DependencyProperty CostProperty = DependencyProperty.Register(
+            "Cost", 
+            typeof(CardCostCollection), 
+            typeof(ManaCostView), 
+            new PropertyMetadata(null)
+        );
+    }
+}
